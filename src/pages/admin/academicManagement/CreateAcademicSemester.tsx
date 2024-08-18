1733622b41615bdfa8371d1a7fb0a9d53b0feb3a
Arifur Rahman <arifur3429@gmail.com>
2024-08-18 18:11:28 +0600

    form select done

--- src/pages/admin/academicManagement/CreateAcademicSemester.tsx
+++ src/pages/admin/academicManagement/CreateAcademicSemester.tsx
@@ -1,17 +1,24 @@
 import { FieldValues, SubmitHandler } from "react-hook-form";
 import PHForm from "../../../components/form/PHForm";
-import PHInput from "../../../components/form/PHInput";
-import { Button } from "antd";
+// import PHInput from "../../../components/form/PHInput";
+import { Button, Col, Flex } from "antd";
+import PHSelect from "../../../components/form/PHSelect";
 
 const CreateAcademicSemester = () => {
   const onsubmit: SubmitHandler<FieldValues> = (data) => {
     console.log(data);
   };
   return (
-    <PHForm onSubmit={onsubmit}>
-      <PHInput type="text" name="name" />
-      <Button htmlType="submit"> Submit</Button>
-    </PHForm>
+    <Flex justify="center" align="center">
+      <Col span={6}>
+        <PHForm onSubmit={onsubmit}>
+          {/* <PHInput type="text" name="year" label="year" /> */}
+          {/* <PHInput type="text" name="name" label="name"/> */}
+          <PHSelect label="select" name="name" />
+          <Button htmlType="submit"> Submit</Button>
+        </PHForm>
+      </Col>
+    </Flex>
   );
 };
 
