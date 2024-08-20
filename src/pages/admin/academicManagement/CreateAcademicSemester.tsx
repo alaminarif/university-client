008bbb42affcfd99a9a7ae222d1a40744646109d
Arifur Rahman <arifur3429@gmail.com>
2024-08-20 17:01:16 +0600

    academic semester  done

--- src/pages/admin/academicManagement/CreateAcademicSemester.tsx
+++ src/pages/admin/academicManagement/CreateAcademicSemester.tsx
@@ -7,38 +7,56 @@ import { semesterOptions } from "../../../constants/semester";
 import { monthOptions } from "../../../constants/global";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { academicSemesterSchema } from "../../../schemas/academicManagement.schema";
+import { useAddAcademicSemesterMutation } from "../../../redux/features/admin/academicManagement.api";
+import { toast } from "sonner";
+import { TResponse } from "../../../types/global";
+import { TAcademicSemester } from "../../../types/academicManagement.type";
 
 const currentYear = new Date().getFullYear();
 const yearOptions = [0, 1, 2, 3, 4].map((number) => ({
   value: String(currentYear + number),
   label: String(currentYear + number),
 }));
-console.log(yearOptions);
+
 const CreateAcademicSemester = () => {
-  const onsubmit: SubmitHandler<FieldValues> = (data) => {
+  const [addAcademicSemester] = useAddAcademicSemesterMutation();
+
+  const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+    const toastId = toast.loading("Creating...");
+
     const name = semesterOptions[Number(data?.name) - 1]?.label;
+
     const semesterData = {
       name,
       code: data.name,
-      year: data,
+      year: data.year,
       startMonth: data.startMonth,
       endMonth: data.endMonth,
     };
-    console.log(semesterData);
+
+    try {
+      const res = (await addAcademicSemester(semesterData)) as TResponse<TAcademicSemester>;
+      console.log(res);
+      if (res.error) {
+        toast.error(res.error.data.message, { id: toastId });
+      } else {
+        toast.success("Semester created", { id: toastId });
+      }
+    } catch (err) {
+      toast.error("Something went wrong", { id: toastId });
+    }
   };
 
-  // const academicSemesterSchema = z.object({
-  //   name: z.string({ required_error: "this field is required" }),
-  // });
   return (
-    <Flex justify="center">
+    <Flex justify="center" align="center">
       <Col span={6}>
-        <PHForm onSubmit={onsubmit} resolver={zodResolver(academicSemesterSchema)}>
+        <PHForm onSubmit={onSubmit} resolver={zodResolver(academicSemesterSchema)}>
           <PHSelect label="Name" name="name" options={semesterOptions} />
           <PHSelect label="Year" name="year" options={yearOptions} />
           <PHSelect label="Start Month" name="startMonth" options={monthOptions} />
           <PHSelect label="End Month" name="endMonth" options={monthOptions} />
-          <Button htmlType="submit"> Submit</Button>
+
+          <Button htmlType="submit">Submit</Button>
         </PHForm>
       </Col>
     </Flex>
